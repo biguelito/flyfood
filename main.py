f5d5c17e5a4fc8970dafb50ee2ef448f960e28b2
gabriel reis <gabrielr.nogueira2000@gmail.com>
2021-11-23 19:21:23 -0300

    atualizando main para testes

--- main.py
+++ main.py
@@ -3,40 +3,38 @@ from brute_force import BruteForce as BF
 from algoritmo_genetico import GeneticAlgoritm as GA
 from utils import Utils
 
-def forca_bruta(distancias):
+def testar_forca_bruta(distancias):
+    start = timeit.default_timer()
     bf = BF(distancias)
+    m_caminho, m_custo = bf.shortest_path()
+    print('forca bruta: ', m_caminho, m_custo)
+    custo_forca_bruta = timeit.default_timer() - start
     
-    menor_caminho, menor_custo = bf.shortest_path()
-    print('forca bruta: ', menor_caminho, menor_custo)
+    return custo_forca_bruta, m_custo, m_caminho
 
-    return
-
-def algoritmo_genetico(distances, quant_populacao, geracoes, prob_mutacao):
-    ag = GA(distances)
-    menor_caminho, menor_custo = ag.search_min_way(quant_populacao, geracoes, prob_mutacao)
-    print('algoritmo genetico: ', menor_caminho, menor_custo)
+def testar_algoritmo_genetico(distancias, quant_populacao, geracoes, prob_mutacao):
+    start = timeit.default_timer()
+    ag = GA(distancias)
+    m_caminho, m_custo = ag.search_min_way(quant_populacao, geracoes, prob_mutacao)
+    print('algoritmo genetico: ', m_caminho, m_custo)
+    custo_algoritmo_genetico = timeit.default_timer() - start
     
-    return
+    return custo_algoritmo_genetico, m_custo, m_caminho
 
 def testar(grafo, tamanho_populacao, geracoes, probabilidade_mutacao):
     distancias = Utils.get_distances(grafo)    
-
-    start = timeit.default_timer()
-    algoritmo_genetico(distancias, tamanho_populacao, geracoes, probabilidade_mutacao)
-    custo_algoritmo_genetico = timeit.default_timer() - start
-
-    start = timeit.default_timer()
-    forca_bruta(distancias)
-    custo_forca_bruta = timeit.default_timer() - start
-
-    print(f'{custo_forca_bruta=}\n{custo_algoritmo_genetico=}')
-    if custo_forca_bruta < custo_algoritmo_genetico:
-        print(f'forca bruta foi mais rapido por {custo_algoritmo_genetico-custo_forca_bruta}\n')
+    custo_fb, m_custo_fb, m_caminho_fb = testar_forca_bruta(distancias)
+    custo_ag, m_custo_ag, m_caminho_ag = testar_algoritmo_genetico(distancias, tamanho_populacao, geracoes, probabilidade_mutacao)
+    
+    print(f'{custo_fb=}\n{custo_ag=}')
+    if custo_fb < custo_ag:
+        print(f'forca bruta foi mais rapido por {custo_ag-custo_fb}\n')
     else:
-        print(f'algoritmo genetico foi mais rapido por {custo_forca_bruta-custo_algoritmo_genetico}\n')
-    return 
+        print(f'algoritmo genetico foi mais rapido por {custo_fb-custo_ag}\n')
+    
+    return custo_ag, custo_ag
 
-def main():
+def testar_grafos():
     grafo = open("input.txt", "r")
     print('testando com o grafo original')
     tamanho_populacao = 4
@@ -69,6 +67,32 @@ def main():
     # testar(grafo, tamanho_populacao, geracoes, probabilidade_mutacao)
     # grafo.close()
 
+    return
 
+def roda_de_teste(n, grafo, tamanho_populacao, geracoes, probabilidade_mutacao):
+    distancias = Utils.get_distances(grafo) 
+    
+    custo_fb, m_custo_fb, m_caminho_fb = testar_forca_bruta(distancias)
+    cfb = (custo_fb, m_custo_fb)
+
+    cag = []
+    for i in range(n):
+        custo_ag, m_custo_ag, m_caminho_ag = testar_algoritmo_genetico(distancias, tamanho_populacao, geracoes, probabilidade_mutacao)
+        cag.append((custo_ag, m_custo_ag))
+
+    return cfb, cag
+
+def main():
+    grafo = open("input.txt", "r")
+    print('testando com o grafo original')
+    tamanho_populacao = 4
+    geracoes = 7
+    probabilidade_mutacao = 0.05
+    custo_forca_bruta, custos_algoritmo_genetico = roda_de_teste(1, grafo, tamanho_populacao, geracoes, probabilidade_mutacao)
+    grafo.close()
+
+    print(custo_forca_bruta)
+    print(custos_algoritmo_genetico)
+    
 if __name__ == "__main__":
     main()
